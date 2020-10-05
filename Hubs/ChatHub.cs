a6e00b3ef91f8206fbdc7663b8f4edca98cbdab4
Andrew Okesokun <okesokunan@accessbankplc.com>
2020-10-05 13:53:43 +0100

    Add project files.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApp.Hubs
+{
+    public class ChatHub: Hub
+    {
+        public async Task SendMessage(string username, string message) {
+             await Clients.All.SendAsync("ReceivedMessage", new { user = username, message = message });
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,38 @@
+﻿let connection = new signalR.HubConnectionBuilder().withUrl("/chats").build();
+
+
+
+// connect  to hub
+connection.start().then(() => console.log("Connected"))
+    .catch((err) => console.log(err));
+
+
+// subscribe to an event method
+connection.on("ReceivedMessage", (data) => {
+    console.log(data);
+    var messageEle = document.querySelector("#messages");
+    var liEle = document.createElement("li");
+
+    liEle.innerHTML = `<b>username: ${data.user} </b> ${data.message} `;
+
+    messageEle.appendChild(liEle);
+
+    //use data;
+});
+
+
+//trigger server method
+
+function send() {
+    let username = document.querySelector("[name=username]").value;
+    let message = document.querySelector("[name =msg]").value;
+
+    if (username != ""  && message !="") {
+
+        connection.invoke("SendMessage",username, message).catch((err) => console.log(err) );
+
+    }
+
+
+
+}
